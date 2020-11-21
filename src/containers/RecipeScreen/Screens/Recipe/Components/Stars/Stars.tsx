11764507a3f425c78511f22cc1fd1e588f428e89
Florian Carre <florian@loota.fr>
2020-11-21 16:02:37 +0100

    add keys

--- src/containers/RecipeScreen/Screens/Recipe/Components/Stars/Stars.tsx
+++ src/containers/RecipeScreen/Screens/Recipe/Components/Stars/Stars.tsx
@@ -9,11 +9,11 @@ const Stars = ({ stars }: { stars: number }): JSX.Element => {
 
   return (
     <StarsContainer>
-      {Array.from(Array(starsDisplayed), () => (
-        <AntDesign name="star" size={18} color={colors.mainBlue} />
+      {Array.from(Array(starsDisplayed), (_, index) => (
+        <AntDesign key={index} name="star" size={18} color={colors.mainBlue} />
       ))}
-      {Array.from(Array(5 - starsDisplayed), () => (
-        <AntDesign name="staro" size={18} color={colors.mainBlue} />
+      {Array.from(Array(5 - starsDisplayed), (_, index) => (
+        <AntDesign key={index} name="staro" size={18} color={colors.mainBlue} />
       ))}
     </StarsContainer>
   );

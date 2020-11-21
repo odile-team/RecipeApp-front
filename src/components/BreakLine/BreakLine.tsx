736f0a96946df0239a07351b7a69dbbc2840ed0b
Florian Carre <florian@loota.fr>
2020-11-21 14:39:29 +0100

    fix sonarcloud issues

--- src/components/BreakLine/BreakLine.tsx
+++ src/components/BreakLine/BreakLine.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import { BreakLineContainer } from './styles';
 
-const BreakLine = ({}): JSX.Element => {
-  return <BreakLineContainer></BreakLineContainer>;
+const BreakLine = (): JSX.Element => {
+  return <BreakLineContainer />;
 };
 
 export default BreakLine;
